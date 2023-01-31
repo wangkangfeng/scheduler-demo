a0ae1e752b378e9ca2bfaf00ea1e4a791e72d121
wangakangfeng <wangkangfeng1@joyintech.com>
2023-01-31 17:31:01 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">调度</router-link>
+      |
+      <router-link to="/about">拖拽</router-link>
     </nav>
     <router-view />
   </div>
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+  padding: 15px;
+  text-align: left;
+  border-bottom: 1px solid #ccc;
 }
 
 nav a {
